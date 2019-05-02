0a0bd4809d58c401367cd11229715586fc86945c
mbenzekri <benzekri.mohamed@gmail.com>
2019-05-02 23:20:24 +0200

    Some getter for step

--- src/Step.js
+++ src/Step.js
@@ -2,8 +2,13 @@
 // use this class to declare a new kind of step for the cloud engine factory 
 StepDecl: class {
     constructor(options) {
-
+        this.options = options;
     }
+    get name() { return this.options.name }
+    get desc() { return this.options.desc }
+    get parameters() { return this.options.parameters }
+    get inputs() { return this.options.inputs }
+    get outputs() { return this.options.outputs }
 }
 // Step class definition 
 // use this class to define the behavior of a new kind of step for the cloud engine factory 
