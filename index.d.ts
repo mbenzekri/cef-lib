b39dc8c91bf9ed2a0f88b7f156017f89ac9ae685
mbenzekri <benzekri.mohamed@gmail.com>
2019-05-11 00:06:47 +0200

    reorganized to create a steps directory

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+import { Declaration, Flowchart, Batch, Step, ParamsMap } from './step';
+export { Declaration, Flowchart, Batch, Step, ParamsMap };
---
+++ index.ts
@@ -0,0 +1,3 @@
+import { Declaration, Flowchart, Batch, Step, ParamsMap} from './step'
+
+export { Declaration, Flowchart, Batch, Step, ParamsMap  }
